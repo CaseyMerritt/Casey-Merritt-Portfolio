16481dbb340352c9e88cb23db9d3dc8458a45a7a
Casey Merritt <caseymzarvin@yahoo.com>
2023-03-19 22:39:57 -0700

    Small Format Changes
    1. Moved Links from main body to Contact me

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {AiFillGithub, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai'
+import Image from 'next/image';
 
 const Contact = () =>{
     return(
@@ -6,6 +8,11 @@ const Contact = () =>{
             <div className='max-w-[1240] m-auto'>
                 <div className='text-center p-10'>
                     <h2 className='text-3xl py-1 text-white'>Contact Me</h2>
+                    <div className='text-5xl flex justify-center gap-16 py-3 text-white'>
+                        <a href = "https://github.com/CaseyMerritt"  target="_blank"><AiFillGithub/></a>
+                        <a href = "https://www.youtube.com/channel/UCY2obIB3KYsucwUHBMRWC-w"  target="_blank"><AiFillYoutube/></a>
+                        <a href = "https://www.linkedin.com/in/caseymerritt/"  target="_blank"><AiFillLinkedin/></a>
+                    </div>
                  </div>
             </div>
         </div>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import {BsFillMoonStarsFill} from 'react-icons/bs';
-import {AiFillGithub, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai'
-import Image from 'next/image';
 
 const Main = () =>{
     return(
         <div className='w-full h-screen text-center'>
             <div className='max-w-[1240] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div>
-                    <h1 className='text-7xl py-2 text-teal-600 font-medium'>Hi, i'm Casey</h1>
-                    <h1 className='text-4xl py-2'>Software Engineer and Computer Science Student</h1>
+                    <h1 className='text-7xl py-2 text-teal-500 font-medium'>Hi, i'm Casey</h1>
+                    <h1 className='text-4xl py-2 text-gray-800'>Software Engineer and Computer Science Student</h1>
                     <ul className='items-center py-5'>
                         <li>
                             <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md' href='/public/CaseyMerrittResume.pdf' download>
@@ -20,11 +17,6 @@ const Main = () =>{
                             </a>
                         </li>
                     </ul>
-                    <div className='text-5xl flex justify-center gap-16 py-3 text-black'>
-                        <a href = "https://github.com/CaseyMerritt"  target="_blank"><AiFillGithub/></a>
-                        <a href = "https://www.youtube.com/channel/UCY2obIB3KYsucwUHBMRWC-w"  target="_blank"><AiFillYoutube/></a>
-                        <a href = "https://www.linkedin.com/in/caseymerritt/"  target="_blank"><AiFillLinkedin/></a>
-                    </div>
                 </div>
             </div>
         </div>
