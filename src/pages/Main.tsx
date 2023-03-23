6f4c25e9de0b9b00166e64e3a8b5cb73e619158e
Casey Merritt <caseymzarvin@yahoo.com>
2023-03-22 23:03:50 -0700

    image fix

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -14,7 +14,7 @@ const Main = () =>{
                             </a>
                         </div>
                         <div className='hover:scale-105 ease-in duration-200'>
-                            <a className='bg-gradient-to-r from-teal-500 to-cyan-500 text-white px-4 py-2 rounded-md' href='../../CaseyMerrittResume.pdf' download>
+                            <a className='bg-gradient-to-r from-teal-500 to-cyan-500 text-white px-4 py-2 rounded-md' href='../../CaseyMerrittResume.pdf' target='_blank' download>
                                 Resume
                             </a>
                         </div>
