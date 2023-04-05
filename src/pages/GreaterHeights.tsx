f101a1bcf6992e916449fd485502ce6272bb196e
TheZarvin <46637768+CaseyMerritt@users.noreply.github.com>
2023-04-04 18:14:33 -0700

    Added Project Page Formatting

--- src/pages/GreaterHeights.tsx
+++ src/pages/GreaterHeights.tsx
@@ -21,15 +21,6 @@ const GreaterHeights = () =>{
                     <p className='text-2xl text-black'>Project</p>
                     <h2 className='text-3xl text-black' >Overview</h2>
                     <p>As a computer science student, my passion for software engineering and development stems from my love for video games. In high school, I taught myself to code using C# and Unity in order to create my own video games. This led me to explore the different aspects of software engineering, such as game development, virtual reality, and mobile applications. My passion for creating immersive and engaging experiences has motivated me to pursue a career in software engineering, where I can continue to develop cutting-edge technology that brings people together.</p> 
-                    <div className='py-5'>
-                        <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-8 py-2 mt-4 mr-8 rounded-md shadow-xl shadow-gray-400' href='/'>
-                            Abstracts
-                        </a>
-                        <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-8 py-2 mt-4 rounded-md shadow-xl shadow-gray-400' href='/'>
-                            Article
-                        </a>
-                    </div>
-                    
                 </div>
 
                 <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
@@ -45,10 +36,53 @@ const GreaterHeights = () =>{
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div>
-                    <button className='underline' type="button" onClick={() => Router.back()}>Back</button>
+            <div className='p-10 max-w-[1240px] mx-auto'>
+                <div className='py-10 grid lg:grid-cols-2 md:grid-cols-4 gap-8 shadow-xl sahdow-gray-400 rounded-xl p-4'>
+                    <div className=''>
+                        <h3 className='text-2xl'>Personal Tasks</h3>
+                        <p>As a computer science student, my passion for software engineering and development stems from my love for video games. In high school, I taught myself to code using C# and Unity in order to create my own video games. This led me to explore the different aspects of software engineering, such as game development, virtual reality, and mobile applications. My passion for creating immersive and engaging experiences has motivated me to pursue a career in software engineering, where I can continue to develop cutting-edge technology that brings people together.</p>
+                    </div>
+                    <img className='fill' src ='/'  alt='/'/>
                 </div>
+
+                <div className='p-2'/>
+
+                <div className='py-10 grid lg:grid-cols-2 md:grid-cols-4 gap-8 shadow-xl sahdow-gray-400 rounded-xl p-4 bg-gradient-to-r from-cyan-500 to-teal-500'>
+                    <img className='fill' src ='/'  alt='/'/>
+                    <div className='text-white'>
+                        <h3 className='text-2xl'>Unity Tech Demo</h3>
+                        <p>As a computer science student, my passion for software engineering and development stems from my love for video games. In high school, I taught myself to code using C# and Unity in order to create my own video games. This led me to explore the different aspects of software engineering, such as game development, virtual reality, and mobile applications. My passion for creating immersive and engaging experiences has motivated me to pursue a career in software engineering, where I can continue to develop cutting-edge technology that brings people together.</p>
+                    </div>
+                </div>
+
+                <div className='p-2'/>
+
+                <div className='py-10 grid lg:grid-cols-2 md:grid-cols-4 gap-8 shadow-xl sahdow-gray-400 rounded-xl p-4'>
+                    <div className=''>
+                        <h3 className='text-2xl'>Godot Integration</h3>
+                        <p>As a computer science student, my passion for software engineering and development stems from my love for video games. In high school, I taught myself to code using C# and Unity in order to create my own video games. This led me to explore the different aspects of software engineering, such as game development, virtual reality, and mobile applications. My passion for creating immersive and engaging experiences has motivated me to pursue a career in software engineering, where I can continue to develop cutting-edge technology that brings people together.</p>
+                    </div>
+                    <img className='fill' src ='/'  alt='/'/>
+                </div>
+            </div>
+
+            <div className=''>
+                <div className='flex justify-center'>
+                    <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-8 py-2 mt-4 mr-8 rounded-md shadow-xl shadow-gray-400' target='_blank' href='../../Abstracts.pdf' download>
+                        Abstracts
+                    </a>
+                    <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-8 py-2 mt-4 rounded-md shadow-xl shadow-gray-400' target='_blank' href='https://www.nasa.gov/centers/armstrong/features/interns-design-interactive-virtual-reality-prototype.html'>
+                        Article
+                    </a>
+                </div>
+            </div>
+
+            <div className='p-5'></div>
+
+            <div className='absolute max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] z-10 py-5 px-10'>
+                    <button className='underline' type="button" onClick={() => Router.back()}>Back</button>
             </div>
         </div>
     )
